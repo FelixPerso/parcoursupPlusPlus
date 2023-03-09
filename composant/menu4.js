c5dd70b0f47260d54edf1bc5eb7e8929378046d7
Felix Brinet <skyfacef7@gmail.com>
2023-03-09 23:52:52 +0100

    test compilateur riot + connexion api

---
+++ composant/menu4.js
@@ -0,0 +1,8 @@
+var menu4 = {
+  css: null,
+  exports: null,
+  template: (template, expressionTypes, bindingTypes, getComponent) => template('<table class="table is-fullwidth is-hoverable"><thead><tr><th id="pos"><abbr title="Nom">Nom</abbr><a id="sort" href="#"><span class="icon"><i class="fas fa-sort"></i></span></a></th><th><abbr title="Ville">Ville</abbr></th><th><abbr title="Dpt">Dpt</abbr></th><th><abbr title="Moyenne">Moyenne</abbr></th><th><abbr title="Sélectivité">Sélectivité</abbr></th></tr></thead><tbody></tbody><tfoot><tr><th id="pos"><abbr title="Nom">Nom</abbr><a id="sort" href="#"><span class="icon"><i class="fas fa-sort"></i></span></a></th><th><abbr title="Ville">Ville</abbr></th><th><abbr title="Dpt">Dpt</abbr></th><th><abbr title="Moyenne">Moyenne</abbr></th><th><abbr title="Sélectivité">Sélectivité</abbr></th></tr></tfoot><tbody></tbody></table>', []),
+  name: 'menu1'
+};
+
+export { menu4 as default };
